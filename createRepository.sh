2c204a1c0647d91a63889ee6f611c17b9a537a14
Waleed <drmaly22@hotmail.com>
2024-04-10 22:19:37 +0300

    add html index and image files

---
+++ createRepository.sh
@@ -0,0 +1,7 @@
+echo "# frontEnd-task5" >> README.md
+git init
+git add .
+git commit -m "add html index and image files"
+git branch -M main
+git remote add origin git@github.com:drmaly/frontEnd-task5.git
+git push -u origin main
